3ca533e257d0aa399c5ebd93a1fb08c922d57ca9
duc19092005 <duc19092005d@gmail.com>
2025-10-09 00:45:17 +0700

    #19 - Add BussinessLogic Enums and Edit The Turple Return

--- api/BussinessLogic/validations/movieValidations/moviesValidation.cs
+++ api/BussinessLogic/validations/movieValidations/moviesValidation.cs
@@ -1,27 +1,28 @@
 using BussinessLogic.dtos.movieDtos;
+using BussinessLogic.Enums;
 using BussinessLogic.Result;
 
 namespace BussinessLogic.validations.movieValidations;
 
 public class moviesValidation
 {
-    public static (bool,string ?message) createValidate(createMovieDto createMovieDto)
+    public static (bool,string ?message , crudStatus crudStatusE) createValidate(createMovieDto createMovieDto)
     {
         if (createMovieDto.movieReleaseDate > DateTime.Now)
         {
-            return (false , "Lỗi Thời gian ra mắt phải lớn hơn hoặc bằng thời gian hện tại");
+            return (false , "Lỗi Thời gian ra mắt phải lớn hơn hoặc bằng thời gian hện tại" , crudStatus.FieldError);
         }else if (createMovieDto.movieEndedData < createMovieDto.movieReleaseDate)
         {
-            return (false , "Lỗi thời gian kết thúc phải lớn hơn thời gian ra mắt");
+            return (false , "Lỗi thời gian kết thúc phải lớn hơn thời gian ra mắt" , crudStatus.FieldError);
         }else if (!createMovieDto.movieVisualFormatIds.Any())
         {
-            return (false , "Lỗi định dạng hình ảnh bị để trống");
+            return (false , "Lỗi định dạng hình ảnh bị để trống" , crudStatus.FieldError);
         }else if (!createMovieDto.movieGenreIds.Any())
         {
-            return (false , "Lỗi thể loại phim bị để trống");
+            return (false , "Lỗi thể loại phim bị để trống" , crudStatus.FieldError);
         }
 
-        return (true, null);
+        return (true, null , crudStatus.SuccessFully);
     }
 
     public static baseResultForCRUD editValidate(editMovieDto editMovieDto)
